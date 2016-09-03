850d61ee6be19b0d50b84442a5364d903b2d5536
ynbenson <yujiro91224@gmail.com>
2016-09-03 13:29:41 -0400

    latest version

--- AmericanMahjong2/AmericanMahjong2/AmericanMahjong.cpp
+++ AmericanMahjong2/AmericanMahjong2/AmericanMahjong.cpp
@@ -489,6 +489,7 @@ void AmericanMahjong::discard_tile_CPU(int i){
     if(i == 1){ cout << "CPU1 DISCARDED "; print_tile(cp1Cards[index]); }
     if(i == 2){ cout << "CPU2 DISCARDED "; print_tile(cp2Cards[index]); }
     if(i == 3){ cout << "CPU3 DISCARDED "; print_tile(cp3Cards[index]); }
+    discardType = (*cards[i])[index]->type + static_cast<char>((*cards[i])[index]->data);
     cards[i]->erase(cards[i]->begin()+index); // 索引indexにあるタイルをベクターから削除
     cout << endl;
     
@@ -547,10 +548,8 @@ int AmericanMahjong::evaluate_yaku(int i){
 int AmericanMahjong::count_type(char type){
     int count = 0;
     char myData;
-    cout << "\naaaaaaaaaaaaa:";
-    cout << type;
     for(int index=0; index<myCards.size(); index++){
-        myData = myCards[index]->type + myCards[index]->data;
+        myData = myCards[index]->type + static_cast<char>(myCards[index]->data);
         cout << myData << ' ';
         if (myData == type)
         {
